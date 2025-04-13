905ca973081a0d345502dfd1630c888144fef00b
talha-anwar <talhaibnanwar@gmail.com>
2025-04-13 11:09:57 +0600

    another useless commit

---
+++ scripts/startup/bl_operators/foo.py
@@ -0,0 +1,8 @@
+def long_function_name_with_lots_of_parameters(param1, param2, param3, param4, param5, param6):
+    if param1 == param2 and param3 > param4 and param5 != param6:
+        print("The conditions are met.")
+    elif param1 != param2 or param3 == param4:
+        print("One of the conditions is met.")
+    else:
+        print("No conditions were met.")
+    return param1 + param2 + param3 + param4
